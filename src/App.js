475e0209b2be51ae0d18b05b4e614cbed360b8a7
Luc <Luc1fer.tms.ig@gmail.com>
2022-12-12 13:14:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MobileOs from "./components/MobileOS";
+
+function App() {
+  return <div>{
+    <div>
+      <MobileOs />
+    </div>
+  }</div>;
+}
+
+export default App;
